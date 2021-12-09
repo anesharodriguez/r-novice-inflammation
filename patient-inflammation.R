313bb9c22c1a2d64c28a75fbdac72923cb15aff7
anesharodriguez <anesharodriguez@email.arizona.edu>
2021-12-09 22:20:47 +0000

    Homework_10

---
+++ patient-inflammation.R
@@ -0,0 +1,23 @@
+package(readr)
+library(readr)
+data01 <- read.csv("data/data/inflammation-01.csv", header = FALSE, sep = ',')
+class(data01)
+dim(data01)
+data01[c(1:4),c(1:10)]
+data01[c(5:10),c(1:10)]
+#first row, all of the columns
+patient_1 <- data01[1, ]
+#max inflammation for patient 1
+max(patient_1)
+#max inflammation for patient 1
+max(data01[1, ])
+patient_2 <- data01[2, ]
+max(patient_2)
+avg_patient_inflammation <- apply(data01, 1, mean)
+avg_day_inflammation <- apply(data01, 2, mean)
+plot(avg_day_inflammation)
+max_day_inflammation <- apply(data01, 2, max)
+plot(max_day_inflammation)
+min_day_inflammation <- apply(data01, 2, min)
+plot(min_day_inflammation)
+
